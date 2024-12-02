2ca8cb8a96d213aa949b0eb2dbc1ed5691d3a167
Roman Baskov <rvbaskov@gmail.com>
2024-12-02 17:52:23 +0300

    add markup body

---
+++ js/01-vars.js
@@ -0,0 +1,49 @@
+console.log(123);
+console.log("hello");
+console.log(1);
+console.log(2);
+
+//інструкція
+//const x = 20 + 20;
+//вираз
+//20 + 20;
+//літерал
+//10;
+//("puma");
+//true;
+
+//const x = 10;
+//const y = 10;
+
+//let x = "Hello";
+//let y = 20;
+//console.log(x);
+//console.log(y);
+
+//дані
+//10
+//-12;
+//"awsdfghhtffhfgjmhmjkh"
+//'wau'
+
+//булеві дані
+//true;
+//false;
+
+//null;
+//undefined;
+//console.log("true");
+//console.log(true);
+
+//let x;
+//let x = 10;
+let x = 18;
+x = 73;
+
+console.log(x);
+
+//let y = undefined;
+
+const y = 15;
+//y = 34;
+console.log(y);
